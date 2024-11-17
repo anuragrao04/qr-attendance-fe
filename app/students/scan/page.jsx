ce0b5753202b386ce9a2f940573b3b6e45969485
anuragrao04 <raoanu2004@gmail.com>
2024-11-17 11:00:09 +0530

    feat: enforce chrome

--- app/students/scan/page.jsx
+++ app/students/scan/page.jsx
@@ -2,9 +2,26 @@
 import { useEffect, useState } from "react";
 import { useSearchParams } from "next/navigation";
 import { Scanner as ScannerComp, outline } from '@yudiel/react-qr-scanner';
-
+import Image from "next/image";
 
 export default function Page() {
+
+
+  // enforce chrome browser only
+  //
+  const [isChrome, setIsChrome] = useState(true);
+  function arraysEqual(arr1, arr2) {
+    return arr1.length === arr2.length && arr1.every((val, index) => val === arr2[index]);
+  }
+  useEffect(() => {
+    const condition = !!window.chrome && arraysEqual(Object.keys(window.chrome), ['loadTimes', 'csi', 'app', 'runtime']);
+    if (!condition) {
+      // it's not chrome
+      setIsChrome(false)
+    }
+  }, [])
+
+
   const searchParams = useSearchParams();
   const srn = searchParams.get("srn");
 
@@ -86,6 +103,31 @@ export default function Page() {
     console.error(err);
     setErrorMessage("Error scanning QR code.");
   };
+
+  if (!isChrome) {
+    return (
+      <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
+        <div className="max-w-md w-full bg-white rounded-lg shadow-lg p-8 text-center">
+          <div className="mb-6 flex items-center justify-center">
+            <Image src="/chrome-icon.png" width={75} height={75} alt="Chrome Icon" />
+          </div>
+          <h1 className="text-2xl font-bold mb-4 text-gray-800">Please Use Google Chrome</h1>
+          <p className="text-gray-600 mb-6">
+            This website is optimized for Google Chrome. To access the site and enjoy the best experience, please switch to
+            Chrome. Incognito mode is not supported.
+          </p>
+          <a
+            href="https://www.google.com/chrome/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="inline-block bg-blue-600 text-white font-semibold py-2 px-4 rounded hover:bg-blue-700 transition-colors"
+          >
+            Download Chrome
+          </a>
+        </div>
+      </div>
+    )
+  }
 
   return (
     <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
